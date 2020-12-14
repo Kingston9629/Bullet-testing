d90ab0f57976f902fb2ebb2c666095d48de0a504
Kingston9629 <74088448+Kingston9629@users.noreply.github.com>
2020-12-14 09:29:02 +0600

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,17 +3,18 @@ var wall,thickness;
 
 function setup() {
   createCanvas(1600,400);
-  bullet = createSprite(50, 200, 50, 25);
+  bullet = createSprite(50, 200, 50, 5);
   thickness=random(22,83);
   wall = createSprite(1200,200, thickness, height/2);
-  wall.shapeColor = (80,80,80);
+  wall.shapeColor = (230,230,230);
   speed=random(223,321);
   weight=random(30,52);
   bullet.velocityX = speed;
+  bullet.shapeColor=color(255);
 }
 
 function draw() {
-  background(0,0,0);
+  background(0);
 
   if(hasCollided(bullet, wall))
   {
